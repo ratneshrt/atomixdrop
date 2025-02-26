65ad1b743b488088d2019da5f622850edc2d7cbd
ratneshrt <ratneshrt19@gmail.com>
2025-02-27 00:28:49 +0530

    fixed mobile view

--- app/components/FileUpload.tsx
+++ app/components/FileUpload.tsx
@@ -13,24 +13,26 @@ interface UploadFileProps{
 export function UploadFile({ onFileUpload, onClick, gotFile, fileName, onDownload }: UploadFileProps) {
 
   return (
-    <div className="w-screen h-screen p-44 mx-auto min-h-96 border border-dashed bg-white dark:bg-black border-neutral-200 dark:border-neutral-800 rounded-lg">
-      <FileUpload onChange={onFileUpload} />
-      <div className="flex items-center justify-center">
-        <button onClick={onClick} className="w-40 h-10 rounded-xl bg-white text-black border border-black  text-sm">Send File</button>
-      </div>
-      {gotFile && (
-        <>
-            <p className="flex items-center justify-center text-white mr-4 font-semibold pt-2">{fileName}</p>
-            <div className="flex justify-center mt-4">
-            <button
-            className="w-40 h-10 rounded-xl bg-white text-black border border-black text-sm"
-            onClick={onDownload}
-            >
-            Download
-            </button>
+    <div className="bg-black w-screen h-screen">
+      <div className="w-full h-full pt-44 pb-44 mx-auto min-h-96 border border-dashed bg-black dark:bg-black border-neutral-200 dark:border-neutral-800 rounded-lg">
+        <FileUpload onChange={onFileUpload} />
+        <div className="flex items-center justify-center">
+          <button onClick={onClick} className="w-40 h-10 rounded-xl bg-white text-black border border-black  text-sm">Send File</button>
         </div>
-      </>
-      )}
+        {gotFile && (
+          <>
+              <p className="flex items-center justify-center text-white mr-4 font-semibold pt-2">{fileName}</p>
+              <div className="flex justify-center mt-4">
+              <button
+              className="w-40 h-10 rounded-xl bg-white text-black border border-black text-sm"
+              onClick={onDownload}
+              >
+              Download
+              </button>
+          </div>
+        </>
+        )}
+      </div>
     </div>
   );
 }

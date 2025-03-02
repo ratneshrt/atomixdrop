444468ebf30e3560461e9868e8c1ed6ac8521696
ratneshrt <ratneshrt19@gmail.com>
2025-03-02 14:14:57 +0530

    fixed file transfer

--- app/components/Room.tsx
+++ app/components/Room.tsx
@@ -186,29 +186,18 @@ function RoomComp(){
     handleReading()
 
     function handleReading(){
-      let bacthCounter = 0
-      let batchBuffer: Uint8Array[] = []
-
       fileReader.addEventListener('load', (e) => {
         if(e && e.target && e.target.result && e.target.result instanceof ArrayBuffer){
           const arrayBuffer = e.target.result
           const chunkBuffer = new Uint8Array(arrayBuffer)
 
-          batchBuffer.push(chunkBuffer)
-          bacthCounter++
 
-          // peer.on('connect', () => {
-          //   console.log('Peer connection established')
-          //   setConnection(true)
-          // })
+          peer.on('connect', () => {
+            console.log('Peer connection established')
+            setConnection(true)
+          })
 
-          if(bacthCounter === BATCH_SIZE){
-            peer.write(Buffer.concat(batchBuffer))
-            batchBuffer = []
-            bacthCounter = 0
-          }
-
-          // peer.write(chunkBuffer)
+          peer.write(chunkBuffer)
           
           offset += e.target.result.byteLength
           
@@ -227,14 +216,10 @@ function RoomComp(){
           if(file.size && offset < file.size){
             readSliceBlob(offset)
           }else{
-            if(batchBuffer.length > 0){
-              peer.write(Buffer.concat(batchBuffer))
-            }
-
-            console.log("Data sent", file)
+            console.log('Data sent', file)
             peer.write(JSON.stringify({
               done: true,
-              fileName: file.name
+              fileName: file.name,
             }))
             setProgress(100)
             setTransferSpeed(0)
